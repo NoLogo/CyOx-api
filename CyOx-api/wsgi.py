5b9e77c27716a2495cff4bf51b097ab5ea81f57e
Philip Gardner <philip@no-logo.co.uk>
2014-02-16 22:04:44 +0000

    Wsgi fix

--- CyOx-api/wsgi.py
+++ CyOx-api/wsgi.py
@@ -17,18 +17,6 @@ import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CyOx-api.settings")
 
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
-
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
-
-
-# dj_static settings
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
