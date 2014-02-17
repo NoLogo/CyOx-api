86571b0fa6381a4b3411c3531889449ef636b0f4
Philip Gardner <philip@no-logo.co.uk>
2014-02-17 09:21:18 +0000

    Admin commented out

--- CyOx-api/urls.py
+++ CyOx-api/urls.py
@@ -1,4 +1,7 @@
-from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls import include, patterns, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 from .api import CoordinateList
@@ -8,5 +11,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', CoordinateList.as_view(), name='coordinate-list'),
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns() + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
